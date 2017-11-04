97fa032bc2e54de6e2e3582d3b666f3008c88cde
Dawid Skurzok <skurzok@gmail.com>
2017-11-04 16:57:01 +0100

    basic region working

--- view.cpp
+++ view.cpp
@@ -5,8 +5,16 @@
 #include <cmath>
 
 using namespace cv;
+using namespace std;
 
 void show_mat(const cv::Mat &image, std::string const &win_name);
+
+ostream & operator<<(ostream & stream, const cv::Vec3b & v)
+{
+    stream << "(" << (int)v[0] << ", " << (int)v[1] << ", " << (int)v[2] << ")";
+    return stream;
+}
+
 
 class EuclideanDistance
 {
@@ -15,10 +23,8 @@ public:
         threshold_(threshold)
     {}
     bool operator()(const cv::Vec3b & base, const cv::Vec3b & candidate)
-    {
-        auto diff = base - candidate;
-        auto dist = std::sqrt(diff[0] * diff[0] + diff[1] * diff[1] + diff[2] * diff[2]);
-        return dist < threshold_;
+    {// http://answers.opencv.org/question/68718/distance-between-images-pixel-by-pixel/
+        return cv::norm(base, candidate) < threshold_;
     }
 private:
     float threshold_;
@@ -30,23 +36,23 @@ cv::Mat findRegion(const cv::Mat image, int x, int y, T metric)
     struct Cord{int x, y;};
     std::vector<Cord> to_check(1, Cord{x, y});
     
-    enum {UNCHECKED, EMPTY=100, FILLED=200};
+    enum {UNCHECKED = -128, FILLED = 127};
     cv::Mat status(image.rows, image.cols, CV_8SC1);
     status.setTo(UNCHECKED);  // https://stackoverflow.com/questions/4337902/how-to-fill-opencv-image-with-one-solid-color
     
-    auto pattern = image.at<char>(x, y);
+    auto pattern = image.at<cv::Vec3b>(x, y);
     
-    int dbg_filed = 0;
+    //int dbg_filed = 0;
     while (to_check.size())
     {
         auto pos = to_check.back();
         to_check.pop_back();
         
         if (status.at<char>(pos.x, pos.y) == UNCHECKED &&            
-            metric(pattern, image.at<char>(pos.x, pos.y)))
+            metric(pattern, image.at<cv::Vec3b>(pos.x, pos.y)))
         {
             status.at<char>(pos.x, pos.y) = FILLED;
-            dbg_filed++;
+            //dbg_filed++;
             if (pos.x - 1 >= 0 && // pos.x - 1 < image.rows &&
                 status.at<char>(pos.x - 1, pos.y) == UNCHECKED)
             {
@@ -70,7 +76,7 @@ cv::Mat findRegion(const cv::Mat image, int x, int y, T metric)
             //TODO: diagonal ?
         }
     }
-    std::cout << "filed " << dbg_filed << std::endl;
+    //std::cout << "filed " << dbg_filed << std::endl;
     return status;
 }
 
@@ -81,7 +87,7 @@ void fast_test()
     
     //show_mat(image, "Input");
 
-    image = findRegion(image, 10, 10, EuclideanDistance(15));
+    image = findRegion(image, 10, 10, EuclideanDistance(50));
 
     show_mat(image, "Output");
 }
